e9c36a26a5c4530354d77b383f23e08044d92eb5
annaadhiambo <annaadhiambo01@gmail.com>
2020-07-20 01:46:49 +0300

    added math in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function nameCalculator(){
     let day = parseInt(d[2]);
     let cc = parseInt(year.slice(0,2));
     let yy = parseInt(year.slice(2,4));
-    daysOfTheWeek =  (( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day ) % 7)
+    daysOfTheWeek =Math.floor(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day ) % 7)
     maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
     femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
     dayys = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
