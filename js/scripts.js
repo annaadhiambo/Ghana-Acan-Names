6ed41a7286b6b1ca42a77b6a1ebdb53f156feb15
annaadhiambo <annaadhiambo01@gmail.com>
2020-07-19 19:51:59 +0300

    added an image in the index.html and calculateddays in the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-var weekDays = (Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday);
-
-var maleNames =(Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame);
-
-var femaleNames =(Akosua,Adwoa,Abenaa, Akua,Yaa, Afua,Ama);
+function nameCalculator(){
+    let date = document.getElementById('date').Value;
+    let d = date.split('-')
+    let year = d[0];
+    let month = parseInt(d[1]);
+    let day = parseInt(d[2]);
+    let cc = parseInt(year.slice(0,2));
+    let yy = parseInt(year.slice(2,4));
+    daysOfTheWeek =  (( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day ) % 7)
+    maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+    femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+    dayys = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    let gender = document.getElementById('gender').value;
+    if (gender === 'male'){
+        alert('Get to know when you were born' + dayys[daysOfTheWeek] + 'and your name in Akan which is' + maleNames[daysOfTheWeek])
+    }
+    else{
+        alert('Get to know when you were born' + dayys[daysOfTheWeek] + 'and your name in Akan which is' + femaleNames[daysOfTheWeek])
+    }
+}
