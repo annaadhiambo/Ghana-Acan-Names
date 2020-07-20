9a48a2a80bd1e63251137e88b72763ad48686bad
annaadhiambo <annaadhiambo01@gmail.com>
2020-07-20 12:51:49 +0300

    corrected an error in the index.html styled css and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
+    var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+    var dayys = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
 function nameCalculator(){
-    let date = document.getElementById('date').Value;
-    let d = date.split('-')
-    let year = d[0];
-    let month = parseInt(d[1]);
-    let day = parseInt(d[2]);
-    let cc = parseInt(year.slice(0,2));
-    let yy = parseInt(year.slice(2,4));
-    daysOfTheWeek =Math.floor(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day ) % 7)
-    maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
-    femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
-    dayys = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-    let gender = document.getElementById('gender').value;
+    var gender = document.getElementById('gender').value; 
+    var date = document.getElementById('date').value;
+    date = new Date(date);
+
     if (gender === 'male'){
-        alert('Get to know when you were born' + dayys[daysOfTheWeek] + 'and your name in Akan which is' + maleNames[daysOfTheWeek])
+        alert(" Since you were born on " + dayys[date.getDay()] + ',' + " your name is " + maleNames[date.getDay()]);
+    }
+    else if(gender === 'female'){
+        alert(" Since you were born on " + dayys[date.getDay()] + ',' + " your name is " + femaleNames[date.getDay()]);
     }
     else{
-        alert('Get to know when you were born' + dayys[daysOfTheWeek] + 'and your name in Akan which is' + femaleNames[daysOfTheWeek])
+        alert("Error: Input your gender or correct date");''
     }
+
+    
 }
+    
+ 
